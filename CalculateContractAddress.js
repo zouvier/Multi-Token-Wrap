9fbadacb9d4c1a45a54c32eb320db3d466c6bad6
Zoumana Cisse <zoumanacisse692@gmail.com>
2021-08-16 22:09:34 -0700

    minor change

--- CalculateContractAddress.js
+++ CalculateContractAddress.js
@@ -6,13 +6,13 @@
 const rlp = require('rlp');
 const keccak = require('keccak');
 
-var nonce = 0xAD; //The nonce must be a hex literal!
-var sender = '0xc8eAD1336325A162d0aa2A8aaf9559BE5609e37C'; //Requires a hex string as input!
+var nonce = 0xAD; //The nonce must be a hex
+var sender = '0xc8eAD1336325A162d0aa2A8aaf9559BE5609e37C'; //Requires a hex string as input
 
 var input_arr = [ sender, nonce ];
 var rlp_encoded = rlp.encode(input_arr);
 
 var contract_address_long = keccak('keccak256').update(rlp_encoded).digest('hex');
 
-var contract_address = contract_address_long.substring(24); //Trim the first 24 characters.
+var contract_address = contract_address_long.substring(24); //Trim the first 24 characters, its all padding
 console.log("contract_address: " + contract_address);
